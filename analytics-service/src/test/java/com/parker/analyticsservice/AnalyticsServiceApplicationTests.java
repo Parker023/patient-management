26f203f481c9166936b08ad2ee42946e189f1be0
Anirudh <amirudhshanmukha2399@gmail.com>
2025-06-02 08:20:13 +0530

    analytics-service created and proto file added for kafka consumer

---
+++ analytics-service/src/test/java/com/parker/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parker.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
