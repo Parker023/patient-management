a66e8368520c659eaf2c63726355f603de732cf6
Anirudh <amirudhshanmukha2399@gmail.com>
2025-07-06 17:56:26 +0530

    add spacing for better readability in PatientResponseDTO

--- patient-service/src/main/java/com/parker/patientservice/dto/PatientResponseDTO.java
+++ patient-service/src/main/java/com/parker/patientservice/dto/PatientResponseDTO.java
@@ -8,21 +8,27 @@ import lombok.Data;
 @Data
 @JacksonXmlRootElement(localName = "patient")
 public class PatientResponseDTO {
+
     @JacksonXmlProperty(localName = "id")
     @CsvBindByName(column = "id")
     private String id;
+
     @CsvBindByName(column = "name")
     @JacksonXmlProperty(localName = "name")
     private String name;
+
     @CsvBindByName(column = "email")
     @JacksonXmlProperty(localName = "email")
     private String email;
+
     @CsvBindByName(column = "gender")
     @JacksonXmlProperty(localName = "gender")
     private String gender;
+
     @CsvBindByName(column = "address")
     @JacksonXmlProperty(localName = "address")
     private String address;
+
     @CsvBindByName(column = "dob")
     @JacksonXmlProperty(localName = "date-of-birth")
     private String dateOfBirth;
