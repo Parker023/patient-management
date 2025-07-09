99de82fc3f6426830988e12eeb741d92d146297a
Anirudh <amirudhshanmukha2399@gmail.com>
2025-07-09 17:33:58 +0530

    add JpaSpecificationExecutor to PatientRepository for dynamic query support and fix minor documentation typo in ExportManager

--- patient-service/src/main/java/com/parker/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/parker/patientservice/repository/PatientRepository.java
@@ -2,12 +2,14 @@ package com.parker.patientservice.repository;
 
 import com.parker.patientservice.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, UUID> {
+public interface PatientRepository extends JpaRepository<Patient, UUID>, JpaSpecificationExecutor<Patient> {
     boolean existsByEmail(String email);
+
     boolean existsByEmailAndIdNot(String email, UUID patientId);
 }
--- patient-service/src/main/java/com/parker/patientservice/service/export/ExportManager.java
+++ patient-service/src/main/java/com/parker/patientservice/service/export/ExportManager.java
@@ -29,7 +29,7 @@ public class ExportManager {
      * @param num         the page number for which data is to be exported, zero-based.
      * @param size        the number of records to be included in each page.
      * @param sortFields  the comma-separated field names by which the data is to be sorted.
-     * @param order       the sorting order, either "ASC" for ascending or "DESC" for descending.
+     * @param order       in the sorting order, either "ASC" for ascending or "DESC" for descending.
      * @return a byte array containing the exported patient data in the specified format.
      */
     public byte[] export(String format, int num, int size, String sortFields, String order) {
