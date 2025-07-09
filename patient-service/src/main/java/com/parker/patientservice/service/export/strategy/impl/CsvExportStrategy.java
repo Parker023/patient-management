b27d2423e259ae4c542686287c92e611d293702a
Anirudh <amirudhshanmukha2399@gmail.com>
2025-07-09 17:02:41 +0530

    update CsvExportStrategy to set NO_QUOTE_CHARACTER, adjust patient data handling, and update request to accept CSV format

--- patient-service/src/main/java/com/parker/patientservice/service/export/strategy/impl/CsvExportStrategy.java
+++ patient-service/src/main/java/com/parker/patientservice/service/export/strategy/impl/CsvExportStrategy.java
@@ -4,6 +4,7 @@ import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import com.parker.patientservice.constants.PatientConstants;
+import com.parker.patientservice.dto.PatientResponseDTO;
 import com.parker.patientservice.dto.wrapper.BaseExportWrapper;
 import com.parker.patientservice.exception.CsvParsingException;
 import com.parker.patientservice.exception.DataNotAvailableException;
@@ -12,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+
+import static com.opencsv.ICSVWriter.NO_QUOTE_CHARACTER;
 
 /**
  * @author shanmukhaanirudhtalluri
@@ -34,9 +38,12 @@ public class CsvExportStrategy implements ExportStrategy {
         }
 
         var writer = new StringWriter();
-        var beanToCsv = new StatefulBeanToCsvBuilder<>(writer).build();
+        var beanToCsv = new StatefulBeanToCsvBuilder<PatientResponseDTO>(writer)
+                .withQuotechar(NO_QUOTE_CHARACTER)
+                .build();
         try {
-            beanToCsv.write(baseExportWrapper.getData());
+            ArrayList<PatientResponseDTO> patientResponseDTOS = new ArrayList<>(baseExportWrapper.getData());
+            beanToCsv.write(patientResponseDTOS);
         } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
             log.error("Error while exporting data to CSV", e);
             throw new CsvParsingException(e);
